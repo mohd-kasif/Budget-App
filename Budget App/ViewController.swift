d947b07d78e5d12392fb362600d320c11058a28a
Mohd Kashif <kashifmd123@gmail.com>
2024-06-23 19:57:34 +0530

    Initial Commit

---
+++ Budget App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Budget App
+//
+//  Created by Mohd Kashif on 23/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
